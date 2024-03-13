32ec9d8716075ec136828daec84bc78f0192de65
IODINOPOL <148897062+NikitaSumin06@users.noreply.github.com>
2024-03-13 11:41:28 +0300

    Add files via upload

---
+++ ЛР1 VS/ЛР1 VS/ЛР1 VS.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string name;
+
+	std::cout << "Введите ваше имя: ";
+	std::cin >> name;
+
+	std::cout << "Привет, " << name << "! Добро пожаловать!\n";
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
